85f34bd7843eeb1c00945900c3eb1092d3522957
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-05-31 11:00:03 +0100

    Create sample system.

---
+++ gateway.tf
@@ -0,0 +1,29 @@
+resource "digitalocean_droplet" "gateway_green" {
+  count = var.server_count
+  image = "ubuntu-20-04-x64"
+  name = "redis-green-version-1.0-${count.index}"
+  region = "fra1"
+  size = "s-1vcpu-1gb"
+  private_networking = true
+  tags = ["gateway_v1_0", "green_v1_0"]
+  ssh_keys = [29902027]
+  vpc_uuid = digitalocean_vpc.redis_example.id
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = var.pvt_key
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx"
+    ]
+  }
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key_file} ./playbooks/server-setup.yml"
+  }
+}
