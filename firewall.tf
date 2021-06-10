7eeb111d1eafd91bc66e4dfbf4f21faca455aa07
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-06-10 21:40:19 +0100

    Attempt at firewall to protected VPC droplets.

---
+++ firewall.tf
@@ -0,0 +1,17 @@
+resource "digitalocean_firewall" "packet_redis" {
+  name = "only-internal-traffic-please"
+
+  droplet_ids = [digitalocean_droplet.redis_green.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = [digitalocean_vpc.redis_example.ip_range]
+  }
+
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "6379"
+    source_addresses = [digitalocean_vpc.redis_example.ip_range]
+  }
+}
