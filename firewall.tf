f3ca383751bc45e5cc2f3bebf66c5fb9d031122f
s1lv3rsph3r3 <tim.a.rawlins@gmail.com>
2021-06-10 21:44:30 +0100

    Declare count.index for resources.

--- firewall.tf
+++ firewall.tf
@@ -1,17 +1,17 @@
 resource "digitalocean_firewall" "packet_redis" {
   name = "only-internal-traffic-please"
 
-  droplet_ids = [digitalocean_droplet.redis_green.id]
+  droplet_ids = [digitalocean_droplet.redis_green[count.index].id]
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = [digitalocean_vpc.redis_example.ip_range]
+    source_addresses = [digitalocean_vpc.redis_example[count.index].ip_range]
   }
 
   inbound_rule {
     protocol = "tcp"
     port_range = "6379"
-    source_addresses = [digitalocean_vpc.redis_example.ip_range]
+    source_addresses = [digitalocean_vpc.redis_example[count.index].ip_range]
   }
 }
